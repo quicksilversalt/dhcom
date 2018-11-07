d084cf724169cb4ee2c0f236f79e0cd8f4d322a1
Danny Hobart <dannyhobart@Admins-MacBook-Pro-2.local>
2018-11-06 23:16:09 -0500

    adding css framework, adding three primary components, header footer content

--- dannyhobart/src/app/app.module.ts
+++ dannyhobart/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ClarityModule } from '@clr/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderNavComponent } from './header-nav/header-nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyContentComponent } from './body-content/body-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderNavComponent,
+    FooterComponent,
+    BodyContentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ClarityModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
